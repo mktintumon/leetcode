48397deb06350a691102e76a02f1fac2e8298e11
MOHIT KUMAR <41851742+mktintumon@users.noreply.github.com>
2022-04-11 16:39:43 +0530

    Time: 36 ms (26.95%), Space: 75.4 MB (68.99%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,16 +1,38 @@
-class Solution {
-    public int findDuplicate(int[] nums) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        
-        for(int i=0 ; i<nums.length ; i++){
-            if(hm.containsKey(nums[i])) return nums[i];
-            
-            hm.put(nums[i] , hm.getOrDefault(nums[i] , 0) + 1);
-        }
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         HashMap<Integer,Integer> hm = new HashMap<>();
         
 //         for(int i=0 ; i<nums.length ; i++){
+//             if(hm.containsKey(nums[i])) return nums[i];
             
+//             hm.put(nums[i] , hm.getOrDefault(nums[i] , 0) + 1);
 //         }
-        return -1;
+        
+//         return -1;
+//     }
+// }
+
+class Solution {
+ public static int findDuplicate(int[] nums) {
+        int len = nums.length;
+        int low = 1;
+        int high = len - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int cnt = 0;
+            for (int i = 0; i < len; i++) {
+                if (nums[i] <= mid) {
+                    cnt++;
+                }
+            }
+
+            if (cnt <= mid) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
     }
 }
