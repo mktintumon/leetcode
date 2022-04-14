6b79a37dc4a54fe0acdbebcde2cebceb01891ff1
MOHIT KUMAR <41851742+mktintumon@users.noreply.github.com>
2022-04-14 19:12:29 +0530

    Time: 1 ms (100.00%), Space: 43.1 MB (82.21%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,37 +1,49 @@
+// class Solution {
+//     public boolean canJump(int[] nums) {
+//         int[] dp = new int[nums.length];
+//         Arrays.fill(dp,-1);
+//         return helper(0 , nums , dp);
+//     }
+    
+//     public boolean helper(int idx , int[] nums , int[] dp){
+//         if(idx >= nums.length-1)
+//             return true;
+        
+//         if(nums[idx] == 0){
+//             dp[idx] = 0;
+//             return false;
+//         }
+        
+//         if(dp[idx] != -1){
+//             if(dp[idx] == 0) return false;
+            
+//             if(dp[idx] == 1) return true;
+//         }
+        
+//         for(int step=1 ; step<=nums[idx] ; step++){
+//             if(helper(idx+step , nums , dp) == true){
+//                 dp[idx] = 1;
+//                 return true;
+//             }
+//         }
+        
+//         dp[idx] = 0;
+//         return false;
+//     }
+// }
+
+
 class Solution {
     public boolean canJump(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-        return helper(0 , nums , dp);
-    }
-    
-    public boolean helper(int idx , int[] nums , int[] dp){
-        if(idx >= nums.length-1)
-            return true;
-        
-        if(nums[idx] == 0){
-            dp[idx] = 0;
-            return false;
-        }
-        
-        if(dp[idx] != -1){
-            if(dp[idx] == 0) return false;
-            
-            if(dp[idx] == 1) return true;
-        }
-        
-        for(int step=1 ; step<=nums[idx] ; step++){
-            if(helper(idx+step , nums , dp) == true){
-                dp[idx] = 1;
-                return true;
+        int lastGoodindex = nums.length - 1;
+        for(int i = nums.length - 1; i >= 0; i--){
+            if(i + nums[i] >= lastGoodindex){
+                lastGoodindex = i;
             }
         }
-        
-        dp[idx] = 0;
-        return false;
+        return lastGoodindex == 0;
     }
 }
-
 
     
   
