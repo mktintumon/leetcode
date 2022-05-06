e0552159a009e31b881f7130b314392b4801664b
MOHIT KUMAR <41851742+mktintumon@users.noreply.github.com>
2022-05-06 14:28:44 +0530

    Time: 1 ms (78.58%), Space: 43.4 MB (40.98%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,3 +1,5 @@
+/*       RECURSIVE APPROACH
+
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
@@ -22,3 +24,32 @@ class Solution {
        helper(nums , idx+1 , exclude , ans);
    }
 }
+
+*/
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>()); // empty set
+        
+        // looping over nums array for generating subsets
+        for(int e : nums){
+            int size = ans.size();
+            
+            for(int i=0 ; i<size ; i++){
+                
+                //clone the ans arraylist and generate new subset 
+                List<Integer> curr = new ArrayList<>(ans.get(i));
+                
+                // add to current
+                curr.add(e);
+                
+                // add to ans arraylist
+                ans.add(curr);
+            }
+        }
+        
+        return ans;
+    }
+}
+
