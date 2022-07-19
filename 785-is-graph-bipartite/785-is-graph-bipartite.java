b27c1742f3a2f93dccdef95e4815d59684564920
MOHIT KUMAR <41851742+mktintumon@users.noreply.github.com>
2022-07-19 21:51:00 +0530

    Time: 1 ms (88.90%), Space: 53.5 MB (68.97%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,48 +1,37 @@
 class Solution {
-    public class pair{
-        int v;
-        int level;
-        
-        pair(int v , int level){
-            this.v = v;
-            this.level = level;
+    public boolean isBipartite(int[][] graph) {
+        int[] visited = new int[graph.length];
+        for(int v=0 ; v<graph.length ; v++){
+            if(visited[v] == 0){
+                //dfs call
+                boolean isBip = dfs(graph , visited , v , 1); // 1--->color
+                if(isBip == false){
+                    return false;
+                }    
+            }
         }
+        return true;
     }
     
-    public boolean isBipartite(int[][] graph) {
-        int n = graph.length;
-        int[] visited = new int[n];
-        Arrays.fill(visited , -1);
+    public boolean dfs(int[][] graph , int[] visited , int v , int color){
+        visited[v] = color;
         
-        for(int v=0 ; v<n ; v++){
-            if(visited[v] == -1){
+        for(int nbr : graph[v]){
+            if(visited[nbr] == 0){
+                boolean ans = dfs(graph , visited , nbr , color*(-1));
+                if(ans == false){
+                    return false;
+                }
+            }
+            else{
+                int presentColor = visited[nbr];
+                int desiredColor = color*(-1);
                 
-                Queue<pair> qu = new LinkedList<>();
-                qu.add(new pair(v , 0));
-                
-                while(qu.size() > 0){
-                    pair rem = qu.remove();
-                    
-                    if(visited[rem.v] != -1){
-                         // check for level -> if different level means odd length cycle
-                         if(rem.level != visited[rem.v]){
-                            return false;
-                         }
-                     }
-                     else{
-                        visited[rem.v] = rem.level;
-                     }
-                    
-                    for(int e : graph[rem.v]){
-                        if(visited[e] == -1){
-                            qu.add(new pair(e , rem.level+1));
-                        }
-                    }
+                if(presentColor != desiredColor){
+                    return false;
                 }
             }
         }
-        
         return true;
-        
     }
 }
