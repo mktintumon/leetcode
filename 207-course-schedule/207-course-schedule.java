0de12bbce0dbd88e8a9b8604ab83f93395d6162f
MOHIT KUMAR <41851742+mktintumon@users.noreply.github.com>
2022-07-27 18:27:31 +0530

    Time: 27 ms (14.06%), Space: 46.1 MB (83.84%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+       int[] topoSort = new int[numCourses];
+        
+        // make array contains indegree count
+        int[] inDegree = new int[numCourses];
+        for(int[] edge : prerequisites){
+            inDegree[edge[1]]++;
+        }
+        
+        // indgree with count 0 ->> add to queue
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        for(int course = 0; course < numCourses; course++){
+            if(inDegree[course] == 0){
+                queue.add(course);
+            }
+        }
+        
+        
+        // printing --> its reverse topoSort --> start idx from last
+        
+        int idx = topoSort.length-1;
+        while(queue.size() > 0){
+            // remove from queue 
+            // print/add 
+            // decrease indegree of nbr  
+            // check for 0 indegree and add to queue
+            
+            int course = queue.remove();
+            topoSort[idx] = course;
+            idx--;
+            
+            for(int[] edge : prerequisites){
+                if(edge[0] == course){
+                    inDegree[edge[1]]--;
+                
+                    if(inDegree[edge[1]] == 0){
+                        queue.add(edge[1]);
+                    }
+                }
+            }
+        }
+        
+        // cycle detected
+        if(idx != -1){
+            return false;
+        }
+        
+        return true;
+    } 
+}
