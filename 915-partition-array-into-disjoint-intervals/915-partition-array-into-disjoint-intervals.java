5b22f7877722f95e0178b2f7665456b1bab7f4f0
MOHIT KUMAR <41851742+mktintumon@users.noreply.github.com>
2022-08-31 17:36:26 +0530

    Time: 9 ms (31.49%), Space: 88.1 MB (69.68%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
@@ -1,36 +1,19 @@
 class Solution {
     public int partitionDisjoint(int[] nums) {
-        int[] leftMax = new int[nums.length];
-        int[] rightMin = new int[nums.length];
+        int pi = 0;        // partition index
+        int mtp = nums[0]; // max till partition
+        int mtt = nums[0]; // max till travel
         
-        //leftMax
-        for(int i=0 ; i<nums.length ; i++){
-            if(i == 0){
-                leftMax[i] = nums[i];
-            }
-            else{
-                leftMax[i] = Math.max(nums[i] , leftMax[i-1]);
+        for(int i=1 ; i<nums.length ; i++){
+            mtt = Math.max(mtt , nums[i]);
+            
+            //Each element in left is <= every element in right.
+            if(nums[i] < mtp){
+                pi = i;
+                mtp = mtt;
             }
         }
         
-        //rightMin --> filling reversely
-        for(int i=nums.length-1 ; i>=0 ; i--){
-            if(i == nums.length-1){
-                rightMin[i] = nums[i];
-            }
-            else{
-                rightMin[i] = Math.min(nums[i] , rightMin[i+1]);
-            }
-        }
-        
-        int ans = -1;
-        for(int i=0 ; i<nums.length-1 ; i++){
-            if(leftMax[i] <= rightMin[i+1]){
-                ans =  i+1;
-                break;
-            }
-        }
-        
-        return ans;
+        return pi + 1; // length
     }
 }
