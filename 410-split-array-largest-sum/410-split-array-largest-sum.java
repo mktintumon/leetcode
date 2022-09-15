008e689c180b8cb921cae4c4648ae087659311e1
MOHIT KUMAR <41851742+mktintumon@users.noreply.github.com>
2022-09-15 11:48:51 +0530

    Time: 7 ms (18.12%), Space: 41.5 MB (69.23%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        int low = Integer.MIN_VALUE;  // lowest sum allowed
+        int high = 0; // highest sum allowed
+        
+        for(int val : nums){
+            low = Math.max(low , val);
+            high += val;
+        }
+        
+        while(low < high){
+           int mid = low + (high - low) / 2; // allowed limit for partition
+            
+            int countParts = 1;
+            int partSum = 0;
+            for(int val : nums){
+                if(partSum + val <= mid){
+                    //partSum + val <= limit  --> allowed
+                    partSum += val;
+                }
+                else{
+                    // limit se bahar chala gya -> break
+                    partSum = val;
+                    countParts++;
+                }
+            }
+            
+            if(countParts > m){
+                // mtlab limit ko increase krna hoga -> for decreasing parts
+                // limit ke niche wala sara value -> jyada partitions hi dega
+                low = mid + 1;
+            }
+            else{
+                high = mid;
+            }
+        }
+        
+        return low;
+    }
+}
